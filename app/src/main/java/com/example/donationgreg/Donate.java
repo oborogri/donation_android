b32b387a41fca83ab01d44fa539b41472d0e6038
Greg Oboroceanu <grigore.oboroceanu@gmail.com>
2016-09-12 10:53:03 +0100

    baseline app with Donate button

---
+++ app/src/main/java/com/example/donationgreg/Donate.java
@@ -0,0 +1,52 @@
+package com.example.donationgreg;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.util.Log;
+
+import com.example.donationgreg.R;
+
+public class Donate extends AppCompatActivity
+{
+
+    private Button donateButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donate);
+
+        donateButton = (Button) findViewById(R.id.donateButton);
+
+        if (donateButton != null) {
+            Log.v("Donate", "Really got the donate button");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_donate, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
